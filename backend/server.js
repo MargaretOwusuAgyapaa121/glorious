5f0834a11b50845b704d2886ff68ec438b3dea84
MargaretOwusuAgyapaa121 <margarermensah123@gmail.com>
2025-11-26 18:40:50 +0000

    updated db.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,9 @@ const bodyParser = require("body-parser");
 const submissionsRoutes = require("./routes/submission");
 
 const app = express();
-const PORT = 5000;
+// const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 
 // Middlewares
 app.use(cors());
